d1c4586b338118a66613564451d6d06eb80e8fe5
dev-sharan <sharan.manish@ymail.com>
2021-05-23 02:22:40 +0000

    added matsidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  toggle() {
+    setTimeout(() => {
+          localStorage.setItem('height', document.getElementById('navcol-1').classList.contains('show') ? document.getElementsByClassName('navbarwrapper')[0]['offsetHeight'] + 5 +'px' : document.getElementsByClassName('navbarwrapper')[0]['offsetHeight'] + 5+'px');
+    }, 300)
+  }
 }
--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -12,4 +12,7 @@ export class CvComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  getHeight() {
+    return localStorage.getItem('height');
+  }
 }
