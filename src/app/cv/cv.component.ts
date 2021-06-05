b3e5daca5760f40d5790b6e127af1aa2583328e8
dev-sharan <sharan.manish@ymail.com>
2021-06-05 13:18:40 +0000

    added cv component

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -1,21 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cv',
   templateUrl: './cv.component.html',
   styleUrls: ['./cv.component.scss']
 })
-export class CvComponent implements OnInit {
-
-  src = 'https://raw.githubusercontent.com/dev-sharan/uploads/main/manish%20angular%20(2).pdf';
+export class CvComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
   getHeight() {
     return localStorage.getItem('height');
   }
 
+  downloadFile(){
+        let link = document.createElement("a");
+        link.download = "Manish Cv";
+        link.href = "assets/ManishAngular.pdf";
+        link.click();
+ }
 }
